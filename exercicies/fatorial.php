0f8f076239bb4d0c70d333c931c94a9b4e5d872d
leticiacareli <leticia_careli@hotmail.com>
2023-08-28 17:25:57 -0300

    Fatorial

---
+++ exercicies/fatorial.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Crie um programa que calcule o fatorial de um número inteiro positivo fornecido pelo usuário. O fatorial de um número N é dado pela multiplicação de todos os números de 1 a N.
+*/
+
+$n = 6;
+$fatorial = 1;
+$aux = 0;
+
+for($i = 1; $i <= $n; $i++)
+{
+    $fatorial *= $i;
+}
+
+var_dump($fatorial);
