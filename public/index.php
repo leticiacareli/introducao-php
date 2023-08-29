ca14580849ec8afc61881280e693df886a84e6fd
leticiacareli <leticia_careli@hotmail.com>
2023-08-29 08:11:34 -0300

    Condicionais: if-else

--- public/index.php
+++ public/index.php
@@ -284,3 +284,19 @@ if(!$value2)
 {
     echo 'valor é falsy';
 }
+
+// aula 12 - condicionais if-else
+
+//return
+$isAdmin = true;
+
+if($isAdmin)
+{
+    echo 'é admin';
+    return;
+}
+
+echo 'não é admin';
+
+//operadores ternários
+echo ($isAdmin) ? 'é admin' : 'não é admin';
