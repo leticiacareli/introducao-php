9b25c49c094cb5d01e863a264b2340215f1472ec
leticiacareli <leticia_careli@hotmail.com>
2023-08-30 08:59:34 -0300

    Looping: Continue and Break

--- public/foreach.php
+++ public/foreach.php
@@ -6,3 +6,16 @@ foreach($fruits as $key => $fruit)
 {
     echo $key . ' => ' . $fruit . "\n"; 
 }
+
+//Continue e break
+
+foreach($fruits as $key => $fruit)
+{
+    if($key === 1)
+    {
+        // continue;
+        break;
+    }
+
+    echo $fruit . " ";
+}
