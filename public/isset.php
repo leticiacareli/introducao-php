f0d99c96e03fb52f099dec58de80419961acd19f
leticiacareli <leticia_careli@hotmail.com>
2023-08-30 15:44:14 -0300

    Isset

---
+++ public/isset.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$name = 'Letícia';
+
+//verificando se uma variável existe
+// echo isset($name);
+
+//verificando se uma variável existe dentro de um array
+$person = [
+    'name' => 'Letícia',
+    'age' => 28,
+];
+
+echo isset($person['name']);
